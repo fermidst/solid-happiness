8ad648cc3d95d8e7c5c76bb704eed566a3f5902e
Arthur Sultanov <as@boolqa.com>
2020-05-18 11:24:22 +0300

    products logic connected to firestore

--- Controllers/Products/GetProducts/GetProductsHandler.cs
+++ Controllers/Products/GetProducts/GetProductsHandler.cs
@@ -1,45 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Google.Cloud.Firestore;
 using MediatR;
+using Microsoft.Extensions.Options;
+using SolidHappiness.Configs;
 
 namespace SolidHappiness.Controllers.Products.GetProducts
 {
     public class GetProductsHandler : IRequestHandler<GetProductsRequestDto, GetProductsResponseDto>
     {
+        private readonly IOptions<FirestoreConfig> _options;
+
+        public GetProductsHandler(IOptions<FirestoreConfig> options)
+        {
+            _options = options;
+        }
+
         public async Task<GetProductsResponseDto> Handle(GetProductsRequestDto request,
             CancellationToken cancellationToken)
         {
+            var firestore = await FirestoreDb.CreateAsync(_options.Value.ProjectId);
+            var collection = await firestore.Collection("products").GetSnapshotAsync(cancellationToken);
             return new GetProductsResponseDto
             {
-                Products = new List<ProductDto>
-                {
-                    new ProductDto
+                Products = (from document in collection.Documents
+                    let dictionary = document.ToDictionary()
+                    select new ProductDto
                     {
-                        PublicId = Guid.NewGuid(),
-                        Description = "desc1",
-                        Price = 100,
-                        Image = string.Empty,
-                        Name = "name1"
-                    },
-                    new ProductDto
-                    {
-                        PublicId = Guid.NewGuid(),
-                        Description = "desc123",
-                        Price = 200,
-                        Image = string.Empty,
-                        Name = "name12"
-                    },
-                    new ProductDto
-                    {
-                        PublicId = Guid.NewGuid(),
-                        Description = "desc123",
-                        Price = 300,
-                        Image = string.Empty,
-                        Name = "name123"
-                    }
-                }
+                        Id = document.Id,
+                        Name = dictionary["name"].ToString(),
+                        Price = Convert.ToDecimal(dictionary["price"]),
+                        Image = dictionary["image"].ToString(),
+                        Description = dictionary["description"].ToString()
+                    }).ToList()
             };
         }
     }
--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Google.Cloud.Firestore;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using SolidHappiness.Controllers.Products.GetProduct;
+using Microsoft.Extensions.Options;
+using SolidHappiness.Configs;
 using SolidHappiness.Controllers.Products.GetProducts;
 
 namespace SolidHappiness.Controllers
@@ -11,20 +14,31 @@ namespace SolidHappiness.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly IOptions<FirestoreConfig> _options;
 
-        public ProductsController(IMediator mediator)
+        public ProductsController(IMediator mediator, IOptions<FirestoreConfig> options)
         {
             _mediator = mediator;
+            _options = options;
+        }
+
+        [HttpPost]
+        public async Task AddProducts()
+        {
+            var firestore = await FirestoreDb.CreateAsync(_options.Value.ProjectId);
+            var document = firestore.Collection("products").Document();
+            var product = new Dictionary<string, object>
+            {
+                {"description", ""},
+                {"image", ""},
+                {"name", ""},
+                {"price", 0}
+            };
+            await document.SetAsync(product);
         }
 
         [HttpGet]
         public async Task<GetProductsResponseDto> GetProducts([FromRoute] GetProductsRequestDto request)
-        {
-            return await _mediator.Send(request);
-        }
-
-        [HttpGet("{id}")]
-        public async Task<GetProductResponseDto> GetProduct([FromRoute] GetProductRequestDto request)
         {
             return await _mediator.Send(request);
         }
