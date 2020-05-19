f91d0aacfffc22370631f63e57f19d198e73c68d
Arthur Sultanov <as@boolqa.com>
2020-05-19 08:43:04 +0300

    minor fixes

--- Controllers/Products/GetProducts/GetProductsHandler.cs
+++ Controllers/Products/GetProducts/GetProductsHandler.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Google.Cloud.Firestore;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using SolidHappiness.Configs;
 
@@ -11,16 +12,19 @@ namespace SolidHappiness.Controllers.Products.GetProducts
 {
     public class GetProductsHandler : IRequestHandler<GetProductsRequestDto, GetProductsResponseDto>
     {
+        private readonly ILogger<GetProductsHandler> _logger;
         private readonly IOptions<FirestoreConfig> _options;
 
-        public GetProductsHandler(IOptions<FirestoreConfig> options)
+        public GetProductsHandler(IOptions<FirestoreConfig> options, ILogger<GetProductsHandler> logger)
         {
             _options = options;
+            _logger = logger;
         }
 
         public async Task<GetProductsResponseDto> Handle(GetProductsRequestDto request,
             CancellationToken cancellationToken)
         {
+            _logger.Log(LogLevel.Critical, $"get products handler invoked at {DateTime.UtcNow}");
             var firestore = await FirestoreDb.CreateAsync(_options.Value.ProjectId);
             var collection = await firestore.Collection("products").GetSnapshotAsync(cancellationToken);
             return new GetProductsResponseDto
--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,10 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using Google.Cloud.Firestore;
+﻿using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
-using SolidHappiness.Configs;
 using SolidHappiness.Controllers.Products.GetProducts;
 
 namespace SolidHappiness.Controllers
@@ -14,27 +10,10 @@ namespace SolidHappiness.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly IMediator _mediator;
-        private readonly IOptions<FirestoreConfig> _options;
 
-        public ProductsController(IMediator mediator, IOptions<FirestoreConfig> options)
+        public ProductsController(IMediator mediator)
         {
             _mediator = mediator;
-            _options = options;
-        }
-
-        [HttpPost]
-        public async Task AddProducts()
-        {
-            var firestore = await FirestoreDb.CreateAsync(_options.Value.ProjectId);
-            var document = firestore.Collection("products").Document();
-            var product = new Dictionary<string, object>
-            {
-                {"description", ""},
-                {"image", ""},
-                {"name", ""},
-                {"price", 0}
-            };
-            await document.SetAsync(product);
         }
 
         [HttpGet]
